b0e3db6310a63701e5e8e4af551793dbcc1172b0
4heck <mamleev.rus@gmail.com>
2019-01-21 20:20:12 +0300

    dertbh

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Tag
+from .models import Tag, Post
 from django.core.exceptions import ValidationError
 
 class TagForm(forms.ModelForm):
@@ -8,15 +8,24 @@ class TagForm(forms.ModelForm):
 		fields = ['title', 'slug']
 
 		widgets={
-			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
-			'slug': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите слаг'}),
+			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'})
 
 		}
-	def clean_slug(self):
-		new_slug=self.cleaned_data['slug'].lower()
 
-		if new_slug == 'create':
-			raise ValidationError('Слаг не может быть "create"! Мы его первым заняли :)')
-		if Tag.objects.filter(slug__iexact=new_slug).count():
-			raise ValidationError('Слаг должен быть уникальным. А "{}" уже существует :('.format(new_slug))
-		return new_slug
+	def clean_title(self):
+		new_title=self.cleaned_data['title'].lower()
+
+		if Tag.objects.filter(title__iexact=new_title).count():
+			raise ValidationError('Тег должен быть уникальным. А "{}" уже существует :('.format(new_title))
+		return new_title
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ['title', 'slug', 'body', 'tags']
+
+		widgets = {
+			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+			'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите текст поста'}),
+			'tags': forms.SelectMultiple(attrs={'class': 'form-control', 'placeholder': 'Выберите теги'})
+		}
