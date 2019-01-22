f8bab3a7977d0232fa3bf0da40878ddf61866188
4heck <mamleev.rus@gmail.com>
2019-01-22 16:15:44 +0300

    added admin panel

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 	path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
 	path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
 	path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
-	path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url')
+	path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
+	path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url'),
+	path('post/<str:slug>/delete/', PostDelete.as_view(), name='post_delete_url')
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
+from django.urls import reverse
 from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
+from django.contrib.auth.decorators import login_required
 
 def posts_list(request):
 	posts = Post.objects.all()
@@ -29,13 +31,24 @@ class TagCreate(ObjectCreateMixin, View):
 	model_form = TagForm
 	template = 'blog/tag_create.html'
 	template_index = 'blog/tags_list.html'
+	
+class PostUpdate(ObjectUpdateMixin, View):
+	model = Post
+	model_form = PostForm
+	template = 'blog/post_update.html'
 
 class TagUpdate(ObjectUpdateMixin, View):
 	model = Tag
 	model_form = TagForm
 	template = 'blog/tag_update.html'
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostDelete(ObjectDeleteMixin, View):
 	model = Post
-	model_form = PostForm
-	template = 'blog/post_update.html'
+	template = 'blog/post_delete.html'
+	redirect_url = 'posts_list_url'
+
+class TagDelete(ObjectDeleteMixin, View):
+	model = Tag
+	template = 'blog/tag_delete.html'
+	redirect_url = 'tags_list_url'
+
