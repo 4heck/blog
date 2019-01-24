da03a41ab13f74db12fab59cd3f882e51448c4ef
4heck <mamleev.rus@gmail.com>
2019-01-24 15:51:59 +0300

    final

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post
 from .models import Tag
@@ -13,8 +15,35 @@ from .forms import PostForm
 
 
 def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query)| Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    number_of_objects_on_page = 5
+    paginator = Paginator(posts, number_of_objects_on_page)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {'page_object': page,
+               'is_paginated': is_paginated,
+               'next_url': next_url,
+               'prev_url': prev_url}
+
+    return render(request, 'blog/index.html', context=context)
 
 
 def tags_list(request):
